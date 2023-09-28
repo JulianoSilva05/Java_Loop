485c42387c64560a179de2517819c4daeae9b6db
JulianoSilva05 <julianoqm@gmail.com>
2023-09-28 10:24:50 -0300

    entradaDados

--- Entrada.java
+++ Entrada.java
@@ -1,20 +1,28 @@
 import java.util.Scanner; //importa a class que é responsavel por pegar dados dos dispositivos de entrada
 
 public class Entrada {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner leitor = new Scanner(System.in); //no metodo construtor, coloco o System.in que é responsavel por pegar dados do teclado
+        Scanner leitor = new Scanner(System.in); // no metodo construtor, coloco o System.in que é responsavel por pegar
+                                                 // dados do teclado
 
-        int n1 = 0,n2 = 0, res = 0;
+        int n1 = 0, n2 = 0,n3 = 0, soma = 0;
+        String nome = "";
 
-        System.out.println("Digite o primeiro numero");
+        System.out.println("Digite o nome do aluno:");
+        nome = leitor.nextLine();
+
+        System.out.println("Digite a primeira nota");
         n1 = leitor.nextInt();
-        System.out.println("Digite o segundo numero");
+        System.out.println("Digite a segunda nota");
         n2 = leitor.nextInt();
-        res = n1 + n2;
+        System.out.println("Digite a terceira nota");
+        n3 = leitor.nextInt();   
 
+        soma = (n1 + n2 + n3)/3;
 
-        System.out.printf("A soma de %d com %d é %d",n1,n2,res);
+        
+        
     }
-    
+
 }
